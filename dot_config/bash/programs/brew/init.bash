9002d9cc559c2d6ddd346b7527ff46ab0a28b558
Cole Ramey <rameyc@synergipartners.com>
2023-06-02 14:29:40 -0400

    update brew to only run if command brew exsists

--- dot_config/bash/programs/brew/init.bash
+++ dot_config/bash/programs/brew/init.bash
@@ -1,7 +1,10 @@
-if [ -f "$XDG_CONFIG_HOME/bash/programs/brew/env.bash" ]; then
-    . "$XDG_CONFIG_HOME/bash/programs/brew/env.bash"
-fi
 
-if [ -f "$XDG_CONFIG_HOME/bash/programs/brew/startup.bash" ]; then
-    . "$XDG_CONFIG_HOME/bash/programs/brew/startup.bash"
+if command -v brew > /dev/null; then
+    if [ -f "$XDG_CONFIG_HOME/bash/programs/brew/env.bash" ]; then
+        . "$XDG_CONFIG_HOME/bash/programs/brew/env.bash"
+    fi
+    
+    if [ -f "$XDG_CONFIG_HOME/bash/programs/brew/startup.bash" ]; then
+        . "$XDG_CONFIG_HOME/bash/programs/brew/startup.bash"
+    fi
 fi
