b69f9470ef6855e5dfdd49d5ffcdebd1af0173f9
Cole Ramey <ceramey1997@gmail.com>
2023-06-04 21:17:48 -0500

    remove lazy-lock and change brew startup

--- dot_config/bash/programs/brew/init.bash
+++ dot_config/bash/programs/brew/init.bash
@@ -1,10 +1,9 @@
-
-if command -v brew > /dev/null; then
-    if [ -f "$XDG_CONFIG_HOME/bash/programs/brew/env.bash" ]; then
-        . "$XDG_CONFIG_HOME/bash/programs/brew/env.bash"
-    fi
-    
-    if [ -f "$XDG_CONFIG_HOME/bash/programs/brew/startup.bash" ]; then
-        . "$XDG_CONFIG_HOME/bash/programs/brew/startup.bash"
-    fi
+if [ -f "$XDG_CONFIG_HOME/bash/programs/brew/env.bash" ]; then
+    . "$XDG_CONFIG_HOME/bash/programs/brew/env.bash"
 fi
+
+if [ -f "$XDG_CONFIG_HOME/bash/programs/brew/startup.bash" ]; then
+    . "$XDG_CONFIG_HOME/bash/programs/brew/startup.bash"
+fi
+# if command -v brew > /dev/null; then
+# fi
