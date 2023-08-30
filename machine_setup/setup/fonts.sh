a193c65f3573c52bd42309728c7623525e8a1ac1
Cole Ramey <ceramey1997@gmail.com>
2023-08-29 23:38:30 -0500

    update add fonts

---
+++ machine_setup/setup/fonts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+LATESTTAG=$(curl --silent "https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest" | jq -r .tag_name)
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/$LATESTTAG/CascadiaCode.zip -P $HOME
+unzip $HOME/CascadiaCode.zip $HOME/.local/share/fonts
+fc-cache -fv
+
+
+#https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip
