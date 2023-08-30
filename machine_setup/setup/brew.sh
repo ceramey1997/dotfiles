4039fa101b73f0c74282e16cb479b93e6b582b24
Cole Ramey <ceramey1997@gmail.com>
2023-08-30 00:20:48 -0500

    up it

--- machine_setup/setup/brew.sh
+++ machine_setup/setup/brew.sh
@@ -13,8 +13,6 @@ echo "Installing Homebrew"
 echo ""
 echo "   ** || Installing dependencies... 'build-esential, procps, curl, file, git' || ** "
 
-command sudo apt-get update > /dev/null
-command sudo apt-get install -y build-essential procps curl file git > /dev/null
 command curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 
 echo "Complete... brew installed."
