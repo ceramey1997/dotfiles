c374a3ba0fee2cf9efb5bcf0b99b2e5e1f80c1d1
Cole Ramey <rameyc@synergipartners.com>
2024-03-07 08:13:38 -0500

    update ui

--- .config/nvim/lua/cocodust/plugins/language/typescript.lua
+++ .config/nvim/lua/cocodust/plugins/language/typescript.lua
@@ -19,9 +19,21 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "typescript-tools.nvim" },
     opts = {
       servers = {
+        tsserver = {
+          init_options = {
+            plugins = {
+              {
+                name = "@vue/typescript-plugin",
+                location = "node_modules/@vue/typescript-plugin",
+                language = { "javascript", "typescript", "vue" },
+              },
+            },
+          },
+          filetypes = { "javascript", "typescript", "vue" },
+        },
+        -- ESLint
         eslint = {
           settings = {
             workingDirectory = { mode = "auto" },
@@ -36,52 +48,5 @@ return {
       local nls = require("null-ls")
       vim.list_extend(opts.sources, nls.builtins.formatting.prettierd)
     end
-  },
-  {
-    "pmizio/typescript-tools.nvim",
-    opts = {},
-    config = function(_, opts)
-      require("cocodust.base.lsp_helpers.utils").on_attach(function(client, bufnr)
-        if client.name == "tsserver" then
-          vim.keymap.set(
-            "n",
-            "<leader>oi",
-            "<cmd>TSToolsOrganizeImports<cr>",
-            { buffer = bufnr, desc = "[O]rganize [I]mports" }
-          )
-          vim.keymap.set(
-            "n",
-            "<leader>si",
-            "<cmd>TSToolsSortImports<cr>",
-            { buffer = bufnr, desc = "[S]ort [I]mports" }
-          )
-          vim.keymap.set(
-            "n",
-            "<leader>ru",
-            "<cmd>TSToolsRemoveUnused<cr>",
-            { buffer = bufnr, desc = "[R]emove [U]nused" }
-          )
-          vim.keymap.set(
-            "n",
-            "<leader>lz",
-            "<cmd>TSToolsGoToSourceDefinition<cr>",
-            { buffer = bufnr, desc = "Go To Source Definition" }
-          )
-          vim.keymap.set(
-            "n",
-            "<leader>ri",
-            "<cmd>TSToolsRemoveUnusedImports<cr>",
-            { buffer = bufnr, desc = "[R]emove Unused [I]mports" }
-          )
-          vim.keymap.set(
-            "n",
-            "<leader>am",
-            "<cmd>TSToolsAddMissingImports<cr>",
-            { buffer = bufnr, desc = "[A]dd [M]issing Imports" }
-          )
-        end
-      end)
-      require("typescript-tools").setup(opts)
-    end,
   },
 }
